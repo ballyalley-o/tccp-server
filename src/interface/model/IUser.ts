590ea852c9e1083b022f11071ca9b4797bcdb1ea
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 00:33:57 +1300

    Add IUser and IDefault interfaces

---
+++ src/interface/model/IUser.ts
@@ -0,0 +1,19 @@
+import { Schema } from 'mongoose'
+
+interface IUser {
+  firstname: string
+  lastname: string
+  email: string
+  role: Schema.Types.ObjectId
+  password: string
+  location: string
+  username: string
+  avatar: string
+  cohort: Schema.Types.ObjectId
+  progress: Schema.Types.ObjectId
+  resetPasswordToken: string
+  resetPasswordExpire: Date
+  matchPassword(enteredPassword: string): Promise<boolean>
+}
+
+export default IUser
---
+++ src/interface/model/index.ts
@@ -0,0 +1,2 @@
+export { default as IUser } from '@interface/model/IUser'
+export { default as IDefault } from '@interface/model/IDefault'
