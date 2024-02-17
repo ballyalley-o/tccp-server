701306b56c6d3d5496abc57457ffd1a0c4312cfe
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 00:32:31 +1300

    Refactor response constants in response.ts

--- src/constant/response.ts
+++ src/constant/response.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from 'express'
-// const { GLOBAL } = require('../config')
+import { GLOBAL } from '@config'
 
 const RESPONSE = {
   server: (req: Request, res: Response) => {
     const response = {
-      name: 'The CodeCoach Projct',
+      name: GLOBAL.APP_NAME,
       status: 'Running',
-      API: 'tccp-server',
-      // url: GLOBAL.baseUrl,
-      // port: GLOBAL.port,
-      // environment: GLOBAL.env,
+      appServerName: GLOBAL.APP_SERVER_NAME,
+      version: GLOBAL.API_VERSION,
+      url: GLOBAL.API_URL,
+      port: GLOBAL.PORT,
+      environment: GLOBAL.ENV,
     }
     res.send(response)
   },
@@ -20,6 +21,8 @@ const RESPONSE = {
     200: 'OK: Request fetched',
     201: 'CREATED: Request created',
     204: 'NO CONTENT: The server successfully processed the request but there is no content to send in the response.',
+    COLLECTION_SEED: ' COLLECTION/s SEED SUCCESSFUL 🌱 ',
+    COLLECTION_DESTROYED: ' COLLECTION/s DESTROYED 💥 ',
   },
   error: {
     400: 'BAD REQUEST: Client request is Invalid',
@@ -31,6 +34,9 @@ const RESPONSE = {
     500: 'INTERNAL SERVER ERROR: Server encountered an Unhandled Exception',
     503: 'SERVICE UNAVAILABLE: The server is temporarily unable to handle the Request',
     504: 'GATEWAY TIMEOUT: The server acting as a gateway did not receive a timely response from an upstream server',
+    FAILED_SEED: ' FAILED TO SEED COLLECTION/s SEED ',
+    FAILED_DESTROY: ' FAILED TO DESTROY COLLECTION/s ',
+
     parseErr: (err: any) => `Error parsing JSON: ${err}`,
     NotInstance: 'This class cannot be instantiated',
   },
