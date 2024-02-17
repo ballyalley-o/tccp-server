fef3ef68dd7c3ef99d7896ce4dd7fdb609077d06
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 00:33:14 +1300

    Add new keys and update existing keys in enum Key

--- src/constant/enum/key.ts
+++ src/constant/enum/key.ts
@@ -1,8 +1,25 @@
 enum Key {
-  Production = 'production',
+  // @tag
+  User = 'User',
+
+  Cookie = 'cookie',
   // @error keys
   ObjectId = 'ObjectId',
   CastError = 'CastError',
+  Connected = 'CONNECTED 🟢',
+  NotConnected = 'NOT CONNECTED 🔴',
+  Environment = 'ENVIRONMENT',
+  Production = 'production',
+  Development = 'development',
+
+  // @logger - req
+  ReqMethod = ' Request Method: ',
+  ReqURL = ' Request URL: ',
+  ReqTime = ' Request Time: ',
+  // @logger - server
+  ServerPort = ' SERVER PORT: ',
+  ServerAPIVersion = ' API VERSION: ',
+  ServerStatus = ' SERVER STATUS: ',
 }
 
 export default Key
