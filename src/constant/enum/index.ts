fa551ad6011770a1accef3dc36ecd6fe72dc22e8
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 00:33:10 +1300

    Add new color and argv constants

--- src/constant/enum/index.ts
+++ src/constant/enum/index.ts
@@ -1,2 +1,4 @@
 export { default as Key } from '@constant/enum/key'
 export { default as PathParam } from '@constant/enum/path-param'
+export { default as COLOR } from '@constant/enum/color'
+export { default as ARGV } from '@constant/enum/argv'
