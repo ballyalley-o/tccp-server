51929e8574138739cb0e2eb150c32c22eca41f50
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 00:32:54 +1300

    Add color enum

---
+++ src/constant/enum/color.ts
@@ -0,0 +1,17 @@
+enum COLOR {
+  RED = 'red',
+  BLUE = 'blue',
+  GREEN = 'green',
+  YELLOW = 'yellow',
+  MAGENTA = 'magenta',
+
+  BG_RED = 'bgRed',
+  BG_BLUE = 'bgBlue',
+  BG_GREEN = 'bgGreen',
+  BG_YELLOW = 'bgYellow',
+  BG_MAGENTA = 'bgMagenta',
+
+  RESET = 'reset',
+}
+
+export default COLOR
