9f5f33b52a498ce8385af97db7f47edce157c3f7
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 00:32:17 +1300

    Update global configuration***
    This commit updates the global configuration in the `global.ts` file. Changes include:
    - Updated `APP_NAME` to 'The CodeCoach Project'
    - Updated `APP_SERVER_NAME` to 'tccp-server'
    - Added imports for `oneDay` and `ARGV`
    - Updated `PORT` to default to 3005 if not provided
    - Added JWT related configurations
    - Added database related configurations

--- src/config/global.ts
+++ src/config/global.ts
@@ -1,17 +1,26 @@
 import { conNex } from '@util'
+import { oneDay } from '@constant'
+import { ARGV } from '@constant/enum'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const GLOBAL = {
-  APP_NAME: 'Bobbi',
-  APP_SERVER_NAME: 'bobbi-server',
+  APP_NAME: 'The CodeCoach Projct',
+  APP_SERVER_NAME: 'tccp-server',
   API_URL: conNex(
     process.env.API_URL?.replace('{PORT}', process.env.PORT ?? '') || '',
     process.env.API_VERSION || ''
   ),
   API_VERSION: process.env.API_VERSION || '',
-  PORT: process.env.PORT || '',
+  PORT: Number(process.env.PORT) || 3005,
   ENV: process.env.NODE_ENV || '',
+  // jwt
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXP: oneDay,
+  // @db
+  DB_URI: process.env.DB_URI,
+  DB_HOST: (db: any) => db.connection.host,
+  DB_NAME: (db: any) => db.connection.name,
 }
 
 export default GLOBAL
