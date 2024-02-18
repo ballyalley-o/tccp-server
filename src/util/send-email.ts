422223ebd9119216cef02a384bfe3b94f2198d0d
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-19 03:13:58 +1300

    Update sendEmail function to use environment variables for SMTP configuration

--- src/util/send-email.ts
+++ src/util/send-email.ts
@@ -1,14 +1,24 @@
 import logger from 'logger'
-import nodemailer, { SentMessageInfo } from 'nodemailer'
-import { GLOBAL } from '@config'
+import nodemailer, { SentMessageInfo, TransportOptions } from 'nodemailer'
+import { transporter } from '@config/app-config'
 import { IEmailOptions } from '@interface'
 import { Key } from '@constant/enum'
+import { GLOBAL } from '@config'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const transporter = nodemailer.createTransport(GLOBAL.MAIL)
+// const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: process.env.SMTP_PORT,
+//   auth: {
+//     user: process.env.SMTP_EMAIL,
+//     pass: process.env.SMTP_PASSWORD,
+//   },
+// } as TransportOptions)
 
 const sendEmail = async (options: IEmailOptions) => {
   let message = {
-    from: GLOBAL.MAIL_FROM,
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
     to: options.email,
     subject: options.subject,
     text: options.message,
