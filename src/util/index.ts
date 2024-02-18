4bbf295c89d5cd8744a143b650e2a1476a3354b5
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 19:20:40 +1300

    Add geocoder utility function

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,4 @@
 export { default as conNex } from '@util/connex'
 export { default as ErrorResponse } from '@util/error-response'
 export { default as sendEmail } from '@util/send-email'
+export { default as geocoder } from '@util/geocoder'
