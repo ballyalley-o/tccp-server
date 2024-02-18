6d370b3148a1e906f8cc880b84439ee67c7a447b
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 19:19:56 +1300

    Add Post and Bootcamp models

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,2 +1,4 @@
 export { default as Default } from '@model/Default'
 export { default as User } from '@model/User'
+export { default as Post } from '@model/Course'
+export { default as Bootcamp } from '@model/Bootcamp'
