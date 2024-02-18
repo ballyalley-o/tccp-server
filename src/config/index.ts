484123a8da4d857a8d48f933f31813ec2b0127a2
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 23:59:39 +1300

    Add new config files

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
 import path from 'path'
 export { default as App } from '@config/app-config'
+export { default as setHeader } from '@config/header'
+export { default as connectDb } from '@config/db'
 export { default as GLOBAL } from '@config/global'
 
 export const __dirname = path.resolve()
