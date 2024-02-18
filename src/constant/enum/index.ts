85402cef580fe56e4d0a655d4fe82701d84997bb
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 19:17:29 +1300

    Add Aggregate enum

--- src/constant/enum/index.ts
+++ src/constant/enum/index.ts
@@ -1,4 +1,5 @@
 export { default as Key } from '@constant/enum/key'
 export { default as PathParam } from '@constant/enum/path-param'
+export { default as Aggregate } from '@constant/enum/aggregate'
 export { default as COLOR } from '@constant/enum/color'
 export { default as ARGV } from '@constant/enum/argv'
