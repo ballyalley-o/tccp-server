c72750ebeb2210be61bdf09acca0dd39066ab293
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-18 14:04:01 +1300

    Add main route and linkStudentRoutes

---
+++ src/route/index.ts
@@ -0,0 +1,8 @@
+// import linkProgressRoutes from '@routes/progress'
+// import linkExerciseRoutes from '@routes/exercise'
+
+const mainRoute = (app: any) => {
+  //   linkStudentRoutes(app)
+}
+
+export default mainRoute
