e212b9e236d87581911b040ef50adc13ae7c544b
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-19 08:38:28 +1300

    Fix logger.db() argument in db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ import GLOBAL from '@config/global'
 const connectDb = async (isConnected: boolean) => {
   try {
     const dbConnect = await mongoose.connect(String(GLOBAL.DB_URI))
-    logger.db(GLOBAL.DB_HOST, dbConnect.connection.name, isConnected)
+    logger.db(GLOBAL.DB_HOST(dbConnect), dbConnect.connection.name, isConnected)
   } catch (error: any) {
     logger.error(error.message)
     process.exit()
