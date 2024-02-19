040260fcf6f997971868442574f5d9812ab20bce
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-20 01:55:14 +1300

    Add user property to IRequestExtended interface

--- src/middleware/async-handler.ts
+++ src/middleware/async-handler.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from 'express'
 // import { IResponseExtended } from '@interface'
 
+// interface IRequestExtended extends Request {
+//   user: {
+//     id: string // adjust the type accordingly
+//     // other user properties if needed
+//   }
+// }
+
 type AsyncHandler = (
   req: Request,
   res: Response,
