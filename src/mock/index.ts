13e67d81b8002de2630ab9968d519400ceb49e5d
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-20 02:50:17 +1300

    Add new collections to mock module

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,1 +1,4 @@
 export { default as userCollection } from '@mock/user-collection'
+export { default as bootcampCollection } from '@mock/bootcamp-collection'
+export { default as courseCollection } from '@mock/course-collection'
+export { default as feedbackCollection } from '@mock/feedback-collection'
