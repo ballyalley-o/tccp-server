9315222ac111302180ed8d21d4bbc0745c1bad5d
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-20 17:32:50 +1300

    Add linkAuthRoute to mainRoute

--- src/route/index.ts
+++ src/route/index.ts
@@ -2,9 +2,11 @@ import { linkUserRoute } from '@route/user'
 import { linkBootcampRoute } from '@route/bootcamp'
 import { linkFeedbackRoute } from '@route/feedback'
 import { linkCourseRoute } from '@route/course'
+import { linkAuthRoute } from './auth'
 import { Application } from 'express'
 
 const mainRoute = (app: Application) => {
+  linkAuthRoute(app)
   linkUserRoute(app)
   linkBootcampRoute(app)
   linkFeedbackRoute(app)
