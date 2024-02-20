43175a13b95b65b5a0d39a29435214119e4b22a8
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-20 17:21:38 +1300

    Refactor bootcamp route and add course and feedback routes

--- src/route/bootcamp/bootcamp.ts
+++ src/route/bootcamp/bootcamp.ts
@@ -1,34 +1,36 @@
-import { Router, RequestHandler } from 'express'
+import { Router } from 'express'
+import courseRoute from '@route/course/course'
+import feedbackRoute from '@route/feedback/feedback'
 import { bootcampController } from '@controller'
 import { advancedResult } from '@middleware'
 import { Bootcamp } from '@model'
-import { Key } from '@constant/enum'
+import { Key, PathParam } from '@constant/enum'
 import { protect, authorize } from '@middleware'
 
 const router = Router({ mergeParams: true })
 
-// TODO: re-route refax
-// router.use('/:bootcampId/courses', courseRouter)
-// router.use('/:bootcampId/feedbacks', feedbackRouter)
+/**
+ * @path - {baseUrl}/api/v0.1/bootcamp
+ */
+router.use(PathParam.REDIR_COURSE, courseRoute)
+router.use(PathParam.REDIR_FEEDBACK, feedbackRoute)
+
+router.route(PathParam.DISTANCE).get(bootcampController.getBootcampsInRadius)
 
 router
-  .route('/radius/:zipcode/:distance')
-  .get(bootcampController.getBootcampsInRadius)
-
-router
-  .route('/')
+  .route(PathParam.ORIGIN)
   .get(bootcampController.getBootcamps)
   .post(bootcampController.createBootcamp)
 
 router
-  .route('/')
+  .route(PathParam.ORIGIN)
   .get(
     advancedResult(Bootcamp, Key.CourseVirtual),
     bootcampController.getBootcamps
   )
 
 router
-  .route('/create')
+  .route(PathParam.CREATE)
   .post(
     protect,
     authorize(Key.Trainer, Key.Admin),
@@ -36,7 +38,7 @@ router
   )
 
 router
-  .route('/:id')
+  .route(PathParam.ID)
   .get(bootcampController.getBootcamp)
   .put(
     protect,
@@ -48,3 +50,6 @@ router
     authorize(Key.Trainer, Key.Admin),
     bootcampController.deleteBootcamp
   )
+
+const bootcampRoute = router
+export default bootcampRoute
