86f8ae75ac7ee687fec8626609f2219261f52a43
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-20 14:55:12 +1300

    Add bodyValidator and autoValidate decorators

---
+++ src/decorator/body-validator.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata'
+import { RequestHandler, Request, Response, NextFunction } from 'express'
+import { MetaKey } from '@constant/enum'
+import { RESPONSE } from '@constant'
+
+export function bodyValidator(...keys: string[] | []) {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    Reflect.defineMetadata(MetaKey.validator, keys, target, key)
+  }
+}
+
+export function autoValidate(keys: string): RequestHandler {
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      res.status(422).send(RESPONSE.error[422])
+      return
+    }
+
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send(RESPONSE.error[422])
+        return
+      }
+    }
+
+    next()
+  }
+}
