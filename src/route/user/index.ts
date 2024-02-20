536f721492fc40894b687cb11932e460b58aada3
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-20 17:22:19 +1300

    Refactor user route function name

--- src/route/user/index.ts
+++ src/route/user/index.ts
@@ -2,8 +2,8 @@ import { Application } from 'express'
 import { PathDir } from '@constant'
 import userRoute from '@route/user/user'
 
-const linkUserRoutes = (app: Application) => {
+const linkUserRoute = (app: Application) => {
   app.use(PathDir.USER, userRoute)
 }
 
-export { linkUserRoutes }
+export { linkUserRoute }
