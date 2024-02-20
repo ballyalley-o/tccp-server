87bda7a3841ab78f1443021c5890d3b5063d016d
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-20 17:21:13 +1300

    Refactor UserController methods to use async/await

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -7,68 +7,82 @@ import { RESPONSE } from '@constant'
 import { User } from '@model'
 import { use, LogRequest, controller, get } from '@decorator'
 
+interface IUserController {
+  getUsers: IExpressController
+  getUser: IExpressController
+  createUser: IExpressController
+  updateUser: IExpressController
+  deleteUser: IExpressController
+}
+
 export class UserController {
   //@desc     Get all users
   //@route    GET /api/v0.1/auth/users
   //@access   Private/Admin
-  @use(LogRequest)
-  public static getUsers(
-    _req: Request,
-    res: Response,
-    _next: NextFunction
-  ): void {
-    res.status(200).json((res as IResponseExtended).advancedResult)
-  }
+  // @use(LogRequest)
+  public static getUsers: IExpressController = asyncHandler(
+    async (_req: Request, res: Response, _next: NextFunction) => {
+      res.status(200).json((res as IResponseExtended).advancedResult)
+    }
+  )
 
   //@desc     Get a user
   //@route    GET /api/v0.1/auth/users/:userId
   //@access   Private/Admin
-  @use(LogRequest)
-  public static getUser(req: Request, res: Response, next: NextFunction) {
-    const user = User.findById(req.params.id)
+  // @use(LogRequest)
+  public static getUser: IExpressController = asyncHandler(
+    async (req: Request, res: Response, _next: NextFunction) => {
+      const user = await User.findById(req.params.id)
 
-    res.status(200).json({
-      success: true,
-      message: RESPONSE.success[200],
-      data: user,
-    })
-  }
+      res.status(200).json({
+        success: true,
+        message: RESPONSE.success[200],
+        data: user,
+      })
+    }
+  )
 
-  @use(LogRequest)
-  public static createUser(req: Request, res: Response, next: NextFunction) {
-    const user = User.create(req.body)
+  // @use(LogRequest)
+  public static createUser: IExpressController = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const user = await User.create(req.body)
 
-    res.status(201).json({
-      success: true,
-      message: RESPONSE.success[201],
-      data: user,
-    })
-  }
+      res.status(201).json({
+        success: true,
+        message: RESPONSE.success[201],
+        data: user,
+      })
+    }
+  )
 
-  @use(LogRequest)
-  public static updateUser(req: Request, res: Response, next: NextFunction) {
-    const user = User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    })
+  // @use(LogRequest)
+  public static updateUser: IExpressController = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+      })
 
-    res.status(200).json({
-      success: true,
-      message: RESPONSE.success.UPDATED,
-      data: user,
-    })
-  }
+      res.status(200).json({
+        success: true,
+        message: RESPONSE.success.UPDATED,
+        data: user,
+      })
+    }
+  )
 
-  @use(LogRequest)
-  public static deleteUser(req: Request, res: Response, next: NextFunction) {
-    User.findByIdAndDelete(req.params.id)
+  // @use(LogRequest)
+  public static deleteUser: IExpressController = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      await User.findByIdAndDelete(req.params.id)
 
-    res.status(200).json({
-      success: true,
-      message: RESPONSE.success.DELETED,
-      data: {},
-    })
-  }
+      res.status(200).json({
+        success: true,
+        message: RESPONSE.success.DELETED,
+        data: {},
+      })
+    }
+  )
 }
 //   //@desc     Create a new user
 //   //@route    POST /api/v0.1/auth/users
