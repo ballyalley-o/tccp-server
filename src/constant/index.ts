9a7992acac83a525aeebb4ea071f0f2b62025b02
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-21 20:27:55 +1300

    Update max-age constants in constant/index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,4 +2,19 @@ export { default as RESPONSE } from '@constant/response'
 export { default as PathDir } from '@constant/path-dir'
 export { REGEX, RegexKey } from '@constant/regex'
 export { REMOVE_FIELDS } from '@constant/remove-fields'
-export { oneDay, oneHr, sixHr, thirtyDays, twoHr } from '@constant/max-age'
+export {
+  expire,
+  fiveSecFromNow,
+  oneHr,
+  twoHr,
+  sixHr,
+  oneDay,
+  thirtyDays,
+  oneHrFromNow,
+  twoHrFromNow,
+  sixHrFromNow,
+  oneDayFromNow,
+  twoDaysFromNow,
+  fifteenDaysFromNow,
+  thirtyDaysFromNow,
+} from '@constant/max-age'
