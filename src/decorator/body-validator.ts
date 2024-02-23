041d895e627736868dedd4e00265a16654b1457a
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-23 23:33:51 +1300

    Update bodyValidator to use Code enum for HTTP status codes

--- src/decorator/body-validator.ts
+++ src/decorator/body-validator.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata'
 import { RequestHandler, Request, Response, NextFunction } from 'express'
-import { MetaKey } from '@constant/enum'
+import { MetaKey, Code } from '@constant/enum'
 import { RESPONSE } from '@constant'
 
 export function bodyValidator(...keys: string[] | []) {
@@ -12,13 +12,13 @@ export function bodyValidator(...keys: string[] | []) {
 export function autoValidate(keys: string): RequestHandler {
   return function (req: Request, res: Response, next: NextFunction) {
     if (!req.body) {
-      res.status(422).send(RESPONSE.error[422])
+      res.status(Code.UNPROCESSABLE_ENTITY).send(RESPONSE.error[422])
       return
     }
 
     for (let key of keys) {
       if (!req.body[key]) {
-        res.status(422).send(RESPONSE.error[422])
+        res.status(Code.UNPROCESSABLE_ENTITY).send(RESPONSE.error[422])
         return
       }
     }
