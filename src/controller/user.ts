4a77ccecda6000e6cbb194b6bdb092a8ca10b737
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-23 21:15:24 +1300

    Refactor user controller with decorators

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,77 +1,96 @@
 import 'reflect-metadata'
 import { Request, Response, NextFunction } from 'express'
-import { asyncHandler } from '@middleware'
 import { IResponseExtended } from '@interface'
-import { IExpressController } from '@interface/middleware'
 import { RESPONSE } from '@constant'
 import { User } from '@model'
+import { Code } from '@constant/enum'
+import { use, LogRequest } from '@decorator'
 
 export class UserController {
   //@desc     Get all users
   //@route    GET /api/{apiVer}/auth/users
   //@access   Private/Admin
-  // @use(LogRequest)
-  public static getUsers: IExpressController = asyncHandler(
-    async (_req: Request, res: Response, _next: NextFunction) => {
-      res.status(200).json((res as IResponseExtended).advancedResult)
-    }
-  )
+  @use(LogRequest)
+  public static async getUsers(
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+  ) {
+    res.status(Code.OK).json((res as IResponseExtended).advancedResult)
+  }
 
   //@desc     Get a user
   //@route    GET /api/{apiVer}/auth/users/:userId
   //@access   Private/Admin
-  // @use(LogRequest)
-  public static getUser: IExpressController = asyncHandler(
-    async (req: Request, res: Response, _next: NextFunction) => {
-      const user = await User.findById(req.params.id)
+  @use(LogRequest)
+  public static async getUser(
+    req: Request,
+    res: Response,
+    _next: NextFunction
+  ) {
+    const user = await User.findById(req.params.id)
 
-      res.status(200).json({
-        success: true,
-        message: RESPONSE.success[200],
-        data: user,
-      })
-    }
-  )
+    res.status(Code.OK).json({
+      success: true,
+      message: RESPONSE.success[200],
+      data: user,
+    })
+  }
 
-  // @use(LogRequest)
-  public static createUser: IExpressController = asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-      const user = await User.create(req.body)
+  //@desc     Create a user
+  //@route    POST /api/{apiVer}/auth/user
+  //@access   Private/Admin
+  @use(LogRequest)
+  public static async createUser(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const user = await User.create(req.body)
 
-      res.status(201).json({
-        success: true,
-        message: RESPONSE.success[201],
-        data: user,
-      })
-    }
-  )
+    res.status(Code.CREATED).json({
+      success: true,
+      message: RESPONSE.success[201],
+      data: user,
+    })
+  }
 
-  // @use(LogRequest)
-  public static updateUser: IExpressController = asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-      })
+  //@desc     Update a user
+  //@route    PUT /api/{apiVer}/auth/user/:id
+  //@access   Private/Admin
+  @use(LogRequest)
+  public static async updateUser(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
 
-      res.status(200).json({
-        success: true,
-        message: RESPONSE.success.UPDATED,
-        data: user,
-      })
-    }
-  )
+    res.status(Code.OK).json({
+      success: true,
+      message: RESPONSE.success.UPDATED,
+      data: user,
+    })
+  }
 
-  // @use(LogRequest)
-  public static deleteUser: IExpressController = asyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-      await User.findByIdAndDelete(req.params.id)
+  //@desc     Delete a user
+  //@route    POST /api/{apiVer}/auth/user/:id
+  //@access   Private/Admin
+  @use(LogRequest)
+  public static async deleteUser(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    await User.findByIdAndDelete(req.params.id)
 
-      res.status(200).json({
-        success: true,
-        message: RESPONSE.success.DELETED,
-        data: {},
-      })
-    }
-  )
+    res.status(Code.OK).json({
+      success: true,
+      message: RESPONSE.success.DELETED,
+      data: {},
+    })
+  }
 }
