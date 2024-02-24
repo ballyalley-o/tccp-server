016d9d08b0f70677243db9de4ed168ce6387b556
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-25 01:46:30 +1300

    Add optional HTML content to IEmailOptions interface

--- src/interface/IEmailOptions.ts
+++ src/interface/IEmailOptions.ts
@@ -1,10 +1,13 @@
+import { IHTMLContent } from '@interface'
+
 declare interface IEmailOptions {
   /**
    * Email address to send the email to
    */
   email: string
   subject: string
-  message: string
+  message?: string
+  html?: string | IHTMLContent
 }
 
 export default IEmailOptions
