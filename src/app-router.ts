ddd15876a173d0015bdcc799d57aa53864dedc6a
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-25 19:00:29 +1300

    Refactor app-router.ts to import PathParam and use it in serverRouter()

--- src/app-router.ts
+++ src/app-router.ts
@@ -1,8 +1,7 @@
-import goodlog from 'good-logs'
 import express, { Router, Request, Response } from 'express'
 import { GLOBAL } from '@config'
 import { PathDir, RESPONSE } from '@constant'
-import { Key } from '@constant/enum'
+import { Key, PathParam } from '@constant/enum'
 
 const ENV = Key.Production
 
@@ -19,10 +18,9 @@ class AppRouter {
 
   static serverRouter() {
     if (GLOBAL.ENV === ENV) {
-      // this._router.use(express.static(PathDir.BUILD_LOC))
-      this._router?.get('*', (req: Request, res: Response) =>
-        // res.sendFile(PathDir.BUILD_VIEW)
-        res.status(200).json({ message: RESPONSE.server })
+      this._router.use(express.static(PathDir.BUILD_LOC))
+      this._router?.get(PathParam.ALL, (req: Request, res: Response) =>
+        res.sendFile(PathDir.BUILD_VIEW)
       )
     } else {
       this._router?.get(PathDir.API_ROOT, RESPONSE.server)
