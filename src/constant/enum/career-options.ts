efc0ab3cbd63de8e1eb7d089762e637731675c80
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-25 20:52:14 +1300

    Add CareerOptions enum

---
+++ src/constant/enum/career-options.ts
@@ -0,0 +1,24 @@
+enum CareerOptions {
+  WebDevelopment = 'Web Development',
+  MobileDevelopment = 'Mobile Development',
+  UIUX = 'UI/UX',
+  DataScience = 'Data Science',
+  Business = 'Business',
+  SoftwareEngineering = 'Software Engineering',
+  FrontEndWebDevelopment = 'FRONT-END Web Development',
+  BackEndWebDevelopment = 'BACK-END Web Development',
+  FullStackWebDevelopment = 'FULL_STACK Web Development',
+  DevOps = 'Dev Ops',
+  WebDesignDevelopment = 'Web Design & Development',
+  DataScienceProgram = 'Data Science Program',
+  SoftwareQA = 'Software QA',
+  MasteringWebDesigning = 'Mastering Web Designing',
+  AndroidAppWebDevelopment = 'Android App Web Development',
+  Meta = 'Meta',
+  IBMDataScience = 'IBM Data Science',
+  AppleiOSWebAppsDevelopment = 'Apple iOS Web Apps Development',
+  BackEnd = 'Back-end',
+  Other = 'Other',
+}
+
+export default CareerOptions
