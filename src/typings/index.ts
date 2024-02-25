732453e060092daa4a884ef1ee628f388a057466
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-26 01:02:20 +1300

    🦄 refactor(Refactor database index definitions):

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,3 +1,4 @@
 export type { ExpressCallback } from '@typings/express-callback'
 export type { Model } from '@typings/model'
 export type { AsyncHandler } from '@typings/async-handler'
+export type { IndexType } from '@typings/record-index'
