f953dce58b5ddf1cd2b2f26c13ec3c01973bb28d
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-25 19:00:57 +1300

    Remove commented out code in geocoder.ts

--- src/util/geocoder.ts
+++ src/util/geocoder.ts
@@ -6,14 +6,11 @@ type OptionsExtended = Options & { httpAdapter?: string }
 
 const options = (apiKey: string): OptionsExtended => {
   return {
-    //   provider: GLOBAL.GEOCODER_PROVIDER || 'google',
     provider: Key.MapQuest,
     httpAdapter: Key.HTTPAdapter,
     apiKey: apiKey || '',
     formatter: null,
   }
 }
-
-// const geocoder = NodeGeocoder(options)
 
 export default options
