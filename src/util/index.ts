4c20c519278482ce6901a980fd738f78c81db026
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-26 01:56:02 +1300

    Add DataResponse to util index

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,6 @@
 export { default as conNex } from '@util/connex'
 export { default as ErrorResponse } from '@util/error-response'
+export { default as DataResponse } from '@util/data-response'
 export { default as sendEmail } from '@util/send-email'
 export { default as htmlContent } from '@util/email-template'
 export { default as geocoder } from '@util/geocoder'
