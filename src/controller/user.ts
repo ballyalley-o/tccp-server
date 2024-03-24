73438236c40e0377c2a32e7a00d31c7d224e94e1
bally <bally.lomibao@howickltd.com>
2024-03-25 01:07:12 +1300

    Modify User controller for avatar upload for Avatar folder #46: Refactor UserController methods to add modify avatar upload

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -29,11 +29,7 @@ export class UserController {
   //@route    GET /
   //@access   PRIVATE/Admin
   @use(LogRequest)
-  public static async getUsers(
-    _req: Request,
-    res: Response,
-    _next: NextFunction
-  ) {
+  public static async getUsers(_req: Request, res: Response, _next: NextFunction) {
     res.status(Code.OK).json((res as IResponseExtended).advancedResult)
   }
 
@@ -41,17 +37,13 @@ export class UserController {
   //@route    GET /:id
   //@access   PRIVATE/Admin
   @use(LogRequest)
-  public static async getUser(
-    req: Request,
-    res: Response,
-    _next: NextFunction
-  ) {
+  public static async getUser(req: Request, res: Response, _next: NextFunction) {
     const user = await User.findById(req.params.id)
 
     res.status(Code.OK).json({
       success: true,
       message: RESPONSE.success[200],
-      data: user,
+      data: user
     })
   }
 
@@ -59,41 +51,26 @@ export class UserController {
   //@route    POST /
   //@access   PRIVATE/Admin
   @use(LogRequest)
-  public static async createUser(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
+  public static async createUser(req: Request, res: Response, next: NextFunction) {
     const user = await User.create(req.body)
     const { email, username } = req.body
     const emailExist = await User.findOne({ email })
     const usernameExist = await User.findOne({ username })
 
     if (emailExist) {
-      res
-        .status(Code.FORBIDDEN)
-        .json({ message: RESPONSE.error.ALREADY_EXISTS(email) })
-      return next(
-        new ErrorResponse(RESPONSE.error.ALREADY_EXISTS(email), Code.FORBIDDEN)
-      )
+      res.status(Code.FORBIDDEN).json({ message: RESPONSE.error.ALREADY_EXISTS(email) })
+      return next(new ErrorResponse(RESPONSE.error.ALREADY_EXISTS(email), Code.FORBIDDEN))
     }
 
     if (usernameExist) {
-      res
-        .status(Code.FORBIDDEN)
-        .json({ message: RESPONSE.error.ALREADY_EXISTS(email) })
-      return next(
-        new ErrorResponse(
-          RESPONSE.error.ALREADY_EXISTS(username),
-          Code.FORBIDDEN
-        )
-      )
+      res.status(Code.FORBIDDEN).json({ message: RESPONSE.error.ALREADY_EXISTS(email) })
+      return next(new ErrorResponse(RESPONSE.error.ALREADY_EXISTS(username), Code.FORBIDDEN))
     }
 
     res.status(Code.CREATED).json({
       success: true,
       message: RESPONSE.success[201],
-      data: user,
+      data: user
     })
   }
 
@@ -101,60 +78,37 @@ export class UserController {
   //@route    PUT /:id
   //@access   PRIVATE/Admin
   @use(LogRequest)
-  public static async updateUser(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
+  public static async updateUser(req: Request, res: Response, next: NextFunction) {
     const user = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true,
+      runValidators: true
     })
 
     if (!user) {
       res.status(Code.NOT_FOUND).json({ message: RESPONSE.error.NOT_FOUND })
-      return next(
-        new ErrorResponse(
-          RESPONSE.error.NOT_FOUND(req.params.id),
-          Code.NOT_FOUND
-        )
-      )
+      return next(new ErrorResponse(RESPONSE.error.NOT_FOUND(req.params.id), Code.NOT_FOUND))
     }
 
     if (user.email !== req.body.email) {
       const emailExist = await User.findOne({ email: req.body.email })
       if (emailExist) {
-        res
-          .status(Code.FORBIDDEN)
-          .json({ message: RESPONSE.error.ALREADY_EXISTS(req.body.email) })
-        return next(
-          new ErrorResponse(
-            RESPONSE.error.ALREADY_EXISTS(req.body.email),
-            Code.FORBIDDEN
-          )
-        )
+        res.status(Code.FORBIDDEN).json({ message: RESPONSE.error.ALREADY_EXISTS(req.body.email) })
+        return next(new ErrorResponse(RESPONSE.error.ALREADY_EXISTS(req.body.email), Code.FORBIDDEN))
       }
     }
 
     if (user.username !== req.body.username) {
       const usernameExist = await User.findOne({ username: req.body.username })
       if (usernameExist) {
-        res
-          .status(Code.FORBIDDEN)
-          .json({ message: RESPONSE.error.ALREADY_EXISTS(req.body.username) })
-        return next(
-          new ErrorResponse(
-            RESPONSE.error.ALREADY_EXISTS(req.body.username),
-            Code.FORBIDDEN
-          )
-        )
+        res.status(Code.FORBIDDEN).json({ message: RESPONSE.error.ALREADY_EXISTS(req.body.username) })
+        return next(new ErrorResponse(RESPONSE.error.ALREADY_EXISTS(req.body.username), Code.FORBIDDEN))
       }
     }
 
     res.status(Code.OK).json({
       success: true,
       message: RESPONSE.success.UPDATED,
-      data: user,
+      data: user
     })
   }
 
@@ -162,17 +116,13 @@ export class UserController {
   //@route    POST /:id
   //@access   PRIVATE/Admin
   @use(LogRequest)
-  public static async deleteUser(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
+  public static async deleteUser(req: Request, res: Response, next: NextFunction) {
     await User.findByIdAndDelete(req.params.id)
 
     res.status(Code.OK).json({
       success: true,
       message: RESPONSE.success.DELETED,
-      data: {},
+      data: {}
     })
   }
 
@@ -180,65 +130,42 @@ export class UserController {
   //@route    PUT /:id/avatar
   //@access   PRIVATE
   @use(LogRequest)
-  public static async uploadUserAvatar(
-    req: any,
-    res: Response,
-    next: NextFunction
-  ) {
+  public static async uploadUserAvatar(req: any, res: Response, next: NextFunction) {
     UserController.setUserId(req)
     const avatar = req.files.avatar
     const user = await User.findById(UserController._userId)
 
     if (!user) {
-      return next(
-        new ErrorResponse(
-          RESPONSE.error.NOT_FOUND(UserController._userId),
-          Code.NOT_FOUND
-        )
-      )
+      return next(new ErrorResponse(RESPONSE.error.NOT_FOUND(UserController._userId), Code.NOT_FOUND))
     }
 
     if (!req.files) {
-      return next(
-        new ErrorResponse(RESPONSE.error.FAILED_UPLOAD, Code.BAD_REQUEST)
-      )
+      return next(new ErrorResponse(RESPONSE.error.FAILED_UPLOAD, Code.BAD_REQUEST))
     }
 
     if (!avatar.mimetype.startsWith(Key.Image)) {
-      return next(
-        new ErrorResponse(RESPONSE.error.FAILED_UPLOAD_AVATAR, Code.BAD_REQUEST)
-      )
+      return next(new ErrorResponse(RESPONSE.error.FAILED_UPLOAD_AVATAR, Code.BAD_REQUEST))
     }
 
     if (avatar.size > GLOBAL.MAX_AVATAR_UPLOAD) {
-      return next(
-        new ErrorResponse(
-          RESPONSE.error.FAILED_FILESIZE(NumKey.FIVE_HUNDRED_KB),
-          Code.BAD_REQUEST
-        )
-      )
+      return next(new ErrorResponse(RESPONSE.error.FAILED_FILESIZE(NumKey.FIVE_HUNDRED_KB), Code.BAD_REQUEST))
     }
 
     avatar.name = GLOBAL.AVATAR_FILENAME(user._id, avatar.name)
-    GLOBAL.AVATAR_UPLOAD_MV(avatar, async (error: any) => {
+    GLOBAL.AVATAR_UPLOAD_MV(avatar, user, async (error: any) => {
       goodlog.error(error?.message)
       if (error) {
-        return next(
-          new ErrorResponse(
-            RESPONSE.error.FAILED_UPLOAD,
-            Code.INTERNAL_SERVER_ERROR
-          )
-        )
+        return next(new ErrorResponse(RESPONSE.error.FAILED_UPLOAD, Code.INTERNAL_SERVER_ERROR))
       }
 
       await User.findByIdAndUpdate(UserController._userId, {
-        avatar: avatar.name,
+        avatar: avatar.name
       })
 
       const response = DataResponse.success(
         {
           photo: avatar.name,
-          user: user.firstname,
+          user: user.firstname
         },
         UserController._userId
       )
@@ -246,7 +173,7 @@ export class UserController {
       res.status(Code.OK).json({
         success: true,
         message: RESPONSE.success.AVATAR_UPLOADED,
-        response,
+        response
       })
     })
   }
