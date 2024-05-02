a9acd8723122747f01cce86396701e12615ed2d0
bally <bally.lomibao@howickltd.com>
2024-05-02 23:14:28 +1200

    @in-a-iii as Requested, please review.
     Declare global interfaces and remove unused imports

--- src/decorator/server-status.ts
+++ src/decorator/server-status.ts
@@ -1,5 +1,3 @@
-import { IIsConnected } from '@interface/utility'
-
 function ServerStatus(target: IIsConnected, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor {
   const originalMethod = descriptor.value
 
--- src/util/connex.ts
+++ src/util/connex.ts
@@ -1,5 +1,3 @@
-import { IConNex } from '@interface/utility'
-
 const conNex: IConNex = (...param) => {
   return param.join('')
 }
