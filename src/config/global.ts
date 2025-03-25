1ed268d4fc93f08820bbf36fb085f411ea2e3108
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-25 11:40:30 +1000

    [config]: set default environment to 'development' in global configuration

--- src/config/global.ts
+++ src/config/global.ts
@@ -17,7 +17,7 @@ const GLOBAL = {
   CORS_ALLOWED_ORIGIN: process.env.CORS_ALLOWED_ORIGIN?.split(',') || [],
   API_VERSION        : process.env.API_VERSION || '',
   PORT               : process.env.PORT || 3005,
-  ENV                : process.env.NODE_ENV || '',
+  ENV                : process.env.NODE_ENV || 'development',
   // jwt
   JWT_SECRET         : process.env.JWT_SECRET,
   JWT_EXP            : oneDay,
