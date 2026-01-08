b7bb85b2279fba2ef6d2a26f7e186bfbb2d87778
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-09 01:56:12 +1000

    [formatting]

--- src/constant/enum/schema.ts
+++ src/constant/enum/schema.ts
@@ -1,35 +1,35 @@
 enum SCHEMA {
-  // @bootcamp
-  NAME = 'Please add a bootcamp name/title',
-  MAX_LENGTH_NAME = 'Name can not be more than 50 characters',
-  DESCRIPTION = 'Please add a description',
-  MIN_LENGTH_DESCRIPTION = 'Description can not be less than 20 characters',
-  MAX_LENGTH_DESCRIPTION = 'Description can not be more than 250 characters',
-  URL = 'Please use a valid URL with HTTP or HTTPS',
-  MAX_LENGTH_PHONE = 'Phone number can not be longer than 20 characters',
-  EMAIL = 'Please add a valid email',
-  ADDRESS = 'Please add an address',
-  DURATION = 'Please add a duration',
-  LOCATION_TYPE = 'Point',
+    // @bootcamp
+  NAME                       = 'Please add a bootcamp name/title',
+  MAX_LENGTH_NAME            = 'Name can not be more than 50 characters',
+  DESCRIPTION                = 'Please add a description',
+  MIN_LENGTH_DESCRIPTION     = 'Description can not be less than 20 characters',
+  MAX_LENGTH_DESCRIPTION     = 'Description can not be more than 250 characters',
+  URL                        = 'Please use a valid URL with HTTP or HTTPS',
+  MAX_LENGTH_PHONE           = 'Phone number can not be longer than 20 characters',
+  EMAIL                      = 'Please add a valid email',
+  ADDRESS                    = 'Please add an address',
+  DURATION                   = 'Please add a duration',
+  LOCATION_TYPE              = 'Point',
   LOCATION_COORDINATES_INDEX = '2dsphere',
-  AVERAGE_RATING_MIN = 'Rating must be at least 1',
-  AVERAGE_RATING_MAX = 'Rating must can not be more than 10',
-  DEFAULT_PHOTO = '/upload/no-photo.jpeg',
-  DEFAULT_BADGE = '/upload/badge/no-badge.png',
-  DEFAULT_AVATAR = '/upload/avatar/no-avatar.jpeg',
-  CAREERS = 'Please add one or more careers',
-  // @course
-  COURSE_TITLE = 'Please add a course title',
-  COURSE_WEEK = 'Please add number of weeks',
+  AVERAGE_RATING_MIN         = 'Rating must be at least 1',
+  AVERAGE_RATING_MAX         = 'Rating must can not be more than 10',
+  DEFAULT_PHOTO              = '/upload/no-photo.jpeg',
+  DEFAULT_BADGE              = '/upload/badge/no-badge.png',
+  DEFAULT_AVATAR             = '/upload/avatar/no-avatar.jpeg',
+  CAREERS                    = 'Please add one or more careers',
+      // @course
+  COURSE_TITLE   = 'Please add a course title',
+  COURSE_WEEK    = 'Please add number of weeks',
   COURSE_TUITION = 'Please add a tuition cost',
-  MINIMUM_SKILL = 'Please add a minimum skill',
-  // @feedback
-  FEEDBACK_TITLE = 'Please add a title to your Feedback',
+  MINIMUM_SKILL  = 'Please add a minimum skill',
+    // @feedback
+  FEEDBACK_TITLE  = 'Please add a title to your Feedback',
   FEEDBACK_RATING = 'Please rating is required',
-  // @user
+    // @user
   FIRST_NAME = 'Please provide your first name',
-  LAST_NAME = 'Please provide your family/last name',
-  PASSWORD = 'Please add a password'
+  LAST_NAME  = 'Please provide your family/last name',
+  PASSWORD   = 'Please add a password'
 }
 
 export default SCHEMA
