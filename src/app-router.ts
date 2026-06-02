6e434fdfbc2b69fdb9959046b76018d6f9ab6565
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-06-03 01:01:15 +1000

    [refactor]

--- src/app-router.ts
+++ src/app-router.ts
@@ -2,12 +2,12 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import express, { Router, Request, Response } from 'express'
 import { GLOBAL } from '@config'
-import { PathDir, RESPONSE } from '@constant'
-import { Key, PathParam } from '@constant/enum'
+import { PathDir } from '@route/dir'
+import { Key } from '@constant/enum'
 
-const ENV = Key.Production
+const ENV        = Key.Production
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname  = path.dirname(__filename)
 
 class AppRouter {
   private static _router: Router
@@ -22,11 +22,11 @@ class AppRouter {
 
   static serverRouter() {
     if (GLOBAL.ENV === ENV) {
-      this._router?.get(PathDir.API_ROOT, (req: Request, res: Response) => {
+      this._router?.get(PathDir.API_ROOT, (_req: Request, res: Response) => {
         res.sendFile(path.join(__dirname,  '../public', 'index.api.html'))
       })
     } else {
-      this._router?.get(PathDir.API_ROOT, (req: Request, res: Response) => {
+      this._router?.get(PathDir.API_ROOT, (_req: Request, res: Response) => {
         res.sendFile(path.join(__dirname,  '../public', 'index.api.html'))
       })
     }
