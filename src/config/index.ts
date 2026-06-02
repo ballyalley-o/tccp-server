1acb0d3bad9b3bbd34dca59d7cbedf883ad79a78
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-06-03 00:53:53 +1000

    small

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import path from 'path'
-export { default as App } from '@config/server'
-export { default as connectDb } from '@config/db'
-export { default as GLOBAL } from '@config/global'
+export { default as App } from './server'
+export { default as connectDb } from './db'
+export { default as GLOBAL } from './global'
 
 export const __dirname = path.resolve()
