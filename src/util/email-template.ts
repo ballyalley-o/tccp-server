99bcf72c59a8a0223a73c87bf8d0c104b59e25ca
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-06-03 01:17:53 +1000

    [fix]

--- src/util/email-template.ts
+++ src/util/email-template.ts
@@ -1,7 +1,8 @@
-import { HTML, CONTENT } from '@constant'
-import { PathParam, Key } from '@constant/enum'
 import { GLOBAL } from '@config'
 import writeSync from 'clipboardy'
+import * as PathParam from '@route/dir'
+import { HTML, CONTENT } from '@constant'
+import { Key } from '@constant/enum'
 
 /**
  *  HTML content for password reset
@@ -15,9 +16,9 @@ const htmlContent = (user: IUser, resetToken: string): string | undefined => {
     return
   }
   const resetLink = `${GLOBAL.API_URL}/auth${PathParam.RESET_PASSWORD}/${resetToken}`
-  let result = HTML.PASSWORD_RESET.replace(Key.ResetLink, GLOBAL.API_HOST)
-  result = result.replace(Key.Username, user.firstname)
-  result = result.replace(Key.EmailContent, CONTENT.RESET_PASSWORD_MSG)
+  let   result    = HTML.PASSWORD_RESET.replace(Key.ResetLink, GLOBAL.API_HOST)
+        result    = result.replace(Key.Username, user.firstname)
+        result    = result.replace(Key.EmailContent, CONTENT.RESET_PASSWORD_MSG)
 
   writeSync.writeSync(resetLink)
 
