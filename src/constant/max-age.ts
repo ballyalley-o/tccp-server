5c52358e620f6fabdc0f6cc2685ac2bf8231052f
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-06-03 00:53:07 +1000

    [formatting]

--- src/constant/max-age.ts
+++ src/constant/max-age.ts
@@ -1,13 +1,13 @@
 interface IMaxAge {
   five_sec: number
-  ten_min: number
-  one_hr: number
-  two_hr: number
-  six_hr: number
-  1: number
-  2: number
-  15: number
-  30: number
+  ten_min : number
+  one_hr  : number
+  two_hr  : number
+  six_hr  : number
+  1       : number
+  2       : number
+  15      : number
+  30      : number
 }
 
 /**
@@ -25,38 +25,38 @@ interface IMaxAge {
  */
 const maxAge: IMaxAge = {
   five_sec: 5 * 1000,
-  ten_min: 10 * 60 * 1000,
-  one_hr: 60 * 60 * 1000,
-  two_hr: 2 * 60 * 60 * 1000,
-  six_hr: 6 * 60 * 60 * 1000,
-  1: 24 * 60 * 60 * 1000,
-  2: 2 * 24 * 60 * 60 * 1000,
-  15: 15 * 24 * 60 * 60 * 1000,
-  30: 30 * 24 * 60 * 60 * 1000
+  ten_min : 10 * 60 * 1000,
+  one_hr  : 60 * 60 * 1000,
+  two_hr  : 2 * 60 * 60 * 1000,
+  six_hr  : 6 * 60 * 60 * 1000,
+  1       : 24 * 60 * 60 * 1000,
+  2       : 2 * 24 * 60 * 60 * 1000,
+  15      : 15 * 24 * 60 * 60 * 1000,
+  30      : 30 * 24 * 60 * 60 * 1000
 }
 
-const expire = new Date(0)
-const fiveSec = maxAge.five_sec
-const tenMin = maxAge.ten_min
-const oneHr = maxAge.one_hr
-const twoHr = maxAge.two_hr
-const sixHr = maxAge.six_hr
-const oneDay = maxAge[1]
-const twoDays = maxAge[2]
+const expire      = new Date(0)
+const fiveSec     = maxAge.five_sec
+const tenMin      = maxAge.ten_min
+const oneHr       = maxAge.one_hr
+const twoHr       = maxAge.two_hr
+const sixHr       = maxAge.six_hr
+const oneDay      = maxAge[1]
+const twoDays     = maxAge[2]
 const fifteenDays = maxAge[15]
-const thirtyDays = maxAge[30]
+const thirtyDays  = maxAge[30]
 
 const fromNow = (time: number) => new Date(Date.now() + time)
 
-const fiveSecFromNow = fromNow(fiveSec)
-const tenMinFromNow = fromNow(tenMin)
-const oneHrFromNow = fromNow(oneHr)
-const twoHrFromNow = fromNow(twoHr)
-const sixHrFromNow = fromNow(sixHr)
-const oneDayFromNow = fromNow(oneDay)
-const twoDaysFromNow = fromNow(twoDays)
+const fiveSecFromNow     = fromNow(fiveSec)
+const tenMinFromNow      = fromNow(tenMin)
+const oneHrFromNow       = fromNow(oneHr)
+const twoHrFromNow       = fromNow(twoHr)
+const sixHrFromNow       = fromNow(sixHr)
+const oneDayFromNow      = fromNow(oneDay)
+const twoDaysFromNow     = fromNow(twoDays)
 const fifteenDaysFromNow = fromNow(fifteenDays)
-const thirtyDaysFromNow = fromNow(thirtyDays)
+const thirtyDaysFromNow  = fromNow(thirtyDays)
 
 export {
   expire,
