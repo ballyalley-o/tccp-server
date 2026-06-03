7bbb81e95792b465aec25870ab2295c95e9b7e46
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-06-04 02:32:15 +1000

    [refactor]: improve type definitions for MongoDB index creation

--- src/db/db.index.ts
+++ src/db/db.index.ts
@@ -1,32 +1,33 @@
-import { IndexType } from '@typings'
 
-const createIndex = <T extends Record<string, number>>(
-  index: T
-): IndexType<T> => {
+
+type MongoIndexValueType = 1 | -1 | '2dsphere' | 'text' | 'hashed'
+type IndexType<T>        = { [K in keyof T]: MongoIndexValueType }
+
+const createIndex = <T extends Record<string, MongoIndexValueType>>(index: T): IndexType<T> => {
   return index as IndexType<T>
 }
 
 const DATABASE_INDEX = {
   FEEDBACK: createIndex({
     bootcamp: 1,
-    user    : 1,
+    user    : 1
   }),
   USER: createIndex({
     email    : 1,
     username : 1,
-    firstname: 1,
+    firstname: 1
   }),
   BOOTCAMP: createIndex({
     averageRating: 1,
-    user: 1,
-    location: '2dsphere'
+    user         : 1,
+    location     : '2dsphere'
   }),
   COURSE: createIndex({
     duration: 1,
     tuition : 1,
     bootcamp: 1,
-    user: 1
-  }),
+    user    : 1
+  })
 }
 
 export default DATABASE_INDEX
