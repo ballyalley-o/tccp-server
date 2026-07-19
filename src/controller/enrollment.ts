bdf32b658b580ed5b90b18136fba6172ef2c2cf2
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-19 18:22:17 +1000

    [feat]: added enrollment controller

---
+++ src/controller/enrollment.ts
@@ -0,0 +1,209 @@
+import goodlog from 'good-logs'
+import { Request, Response, NextFunction } from 'express'
+import { use, LogRequest } from '@decorator'
+import { Enrollment, Bootcamp, Course } from '@model'
+import { Key, Code } from '@constant/enum'
+import { RESPONSE } from '@constant'
+import { ErrorResponse } from '@util'
+
+/**
+ * Enrollment Controller
+ * @path {baseUrl}/api/{apiVer}/enrollment
+ */
+class EnrollmentController {
+  //@desc     Get ALL enrollment
+  //@route    GET /enrollment/me
+  //@access   PRIVATE
+  @use(LogRequest)
+  public static async getEnrollments(req: Request, res: Response, _next: NextFunction) {
+    const userId = req.user.id
+
+    if (userId) {
+      const enrollments = await Enrollment.find({ user: userId }).lean()
+
+      res.status(Code.OK).json({
+        success: true,
+        count  : enrollments.length,
+        data   : enrollments
+      })
+    } else {
+      res.status(Code.OK).json(res.advanceResult)
+    }
+  }
+
+    //@desc     Get single enrollment by id
+    //@route    GET /enrollment/:enrollmentId
+    //@access   PRIVATE
+  @use(LogRequest)
+  public static async getEnrollment(req: Request, res: Response, next: NextFunction) {
+    const enrollmentId = req.params.id
+
+    const enrollment = await Enrollment.findById(enrollmentId)
+      .populate({
+        path  : Key.BootcampVirtual,
+        select: 'bootcamp course status progress'
+      })
+      .lean()
+
+    if (!enrollment) {
+      return next(new ErrorResponse(RESPONSE.error.NOT_FOUND_ENROLLMENT(enrollmentId), (res.statusCode = Code.NOT_FOUND)))
+    }
+
+    res.status(Code.OK).json({
+      success: true,
+      message: RESPONSE.success[200],
+      data   : enrollment
+    })
+  }
+
+  //@desc     Get single course by course id
+  //@route    GET /enrollment/:enrollmentId
+  //@access   PRIVATE
+  @use(LogRequest)
+  public static async getEnrollmentByCourseId(req: Request, res: Response, next: NextFunction) {
+    const courseId = req.params.courseId
+    const course   = await Course.findById(courseId)
+      .populate({
+        path  : Key.BootcampVirtual,
+        select: 'bootcamp course status progress'
+      })
+      .lean()
+
+    if (!course) {
+      return next(new ErrorResponse(RESPONSE.error.NOT_FOUND_COURSE(courseId), (res.statusCode = Code.NOT_FOUND)))
+    }
+
+    res.status(Code.OK).json({
+      success: true,
+      message: RESPONSE.success[200],
+      data   : course
+    })
+  }
+
+  //@desc   Add a course
+  //@route  POST /bootcamp/:bootcampId/course
+  //a@ccess PRIVATE
+  @use(LogRequest)
+  public static async createEnrollment(req: any, res: Response, next: NextFunction) {
+    const bootcampId = req.params.bootcampId
+    const courseId   = req.params.courseId
+    const userId     = req.user.id
+    const userRole   = req.user.role
+
+    const bootcamp = await Bootcamp.findById(bootcampId).select('user').lean()
+    const course   = await Course.findById(courseId).select('bootcamp').lean()
+
+    if (!course) {
+        return next(new ErrorResponse(RESPONSE.error.NOT_FOUND_COURSE(courseId), (res.statusCode = Code.NOT_FOUND)))
+    }
+
+    if (!bootcamp) {
+        return next(new ErrorResponse(RESPONSE.error.NOT_FOUND_BOOTCAMP(bootcampId), (res.statusCode = Code.NOT_FOUND)))
+    }
+
+    if (userRole !== 'user') {
+        return next(new ErrorResponse(RESPONSE.error.NOT_STUDENT(userId), (res.statusCode = Code.UNAUTHORIZED)))
+    }
+
+    try {
+      const enrollment = await Enrollment.create({
+        student       : req.user.id,
+        course        : course._id,
+        bootcamp      : course.bootcamp,
+        status        : 'enrolled',
+        startedAt     : new Date(),
+        lastAccessedAt: new Date()
+      })
+
+      res.status(Code.CREATED).json({
+        success: true,
+        data   : enrollment
+      })
+    } catch (error: any) {
+      goodlog.error(error?.message || error)
+      res.status(Code.BAD_REQUEST).json({
+        success: false,
+        message: error?.message || RESPONSE.error.FAILED_CREATE,
+        error
+      })
+    }
+  }
+
+  //@desc     Update an enrollment
+  //@route    PUT /enrollment/:id
+  //@access   PRIVATE
+  @use(LogRequest)
+  public static async updateEnrollment(req: any, res: Response, next: NextFunction) {
+    const enrollmentId = req.params.id
+    const userId       = req.user.id
+    const userRole     = req.user.role
+
+    const enrollment = await Enrollment.findById(enrollmentId).select('user').lean()
+
+    if (!enrollment) {
+      return next(new ErrorResponse(RESPONSE.error.NOT_FOUND_ENROLLMENT(enrollmentId), (res.statusCode = Code.NOT_FOUND)))
+    }
+
+    if (enrollment.user.toString() !== userId && userRole !== Key.Admin) {
+      return next(new ErrorResponse(RESPONSE.error[401], (res.statusCode = Code.UNAUTHORIZED)))
+    }
+
+    try {
+      const updatedEnrollment = await Enrollment.findByIdAndUpdate(enrollmentId, req.body, {
+        new          : true,
+        runValidators: true
+      })
+
+      res.status(Code.OK).json({
+        success: true,
+        message: RESPONSE.success.UPDATED,
+        data   : updatedEnrollment
+      })
+    } catch (error: any) {
+      goodlog.error(error?.message || error)
+      res.status(Code.BAD_REQUEST).json({
+        success: false,
+        message: error?.message || RESPONSE.error.FAILED_UPDATE,
+        error
+      })
+    }
+  }
+  //@desc     Delete an enrollment
+  //@route    DELETE /enrollment/:id
+  //@access   PRIVATE
+  @use(LogRequest)
+  public static async deleteCourse(req: any, res: Response, next: NextFunction) {
+    const enrollmentId = req.params.id
+    const userId       = req.user.id
+    const userRole     = req.user.role
+
+    const enrollment = await Enrollment.findById(enrollmentId).select('user').lean()
+
+    if (!enrollment) {
+      return next(new ErrorResponse(RESPONSE.error.NOT_FOUND_ENROLLMENT(enrollmentId), (res.statusCode = Code.NOT_FOUND)))
+    }
+
+    if (enrollment.user.toString() !== userId && userRole !== Key.Admin) {
+      return next(new ErrorResponse(RESPONSE.error.NOT_OWNER(userId, enrollmentId), (res.statusCode = Code.UNAUTHORIZED)))
+    }
+
+    try {
+      await Enrollment.deleteOne({ _id: enrollmentId })
+
+      res.status(Code.OK).json({
+        success: true,
+        message: RESPONSE.success.DELETED,
+        data   : {}
+      })
+    } catch (error: any) {
+      goodlog.error(error?.message || error)
+      res.status(Code.BAD_REQUEST).json({
+        success: false,
+        message: error?.message || RESPONSE.error.FAILED_DELETE,
+        error
+      })
+    }
+  }
+}
+
+export default EnrollmentController
--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,6 +1,7 @@
 export { default as AuthController } from './auth'
 export { default as bootcampController } from './bootcamp'
 export { default as CourseController } from './course'
+export { default as EnrollmentController } from './enrollment'
 export { default as FeedbackController } from './feedback'
 export { default as UserController } from './user'
 
