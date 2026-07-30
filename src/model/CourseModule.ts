9d8ea8cefbcb93c0f7090d9af34f488045868525
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-30 15:25:58 +1000

    [feat]: create CourseModule.ts

---
+++ src/model/CourseModule.ts
@@ -0,0 +1,43 @@
+import { Schema, model } from 'mongoose'
+import { Key, SCHEMA }   from '@constant/enum'
+
+const TAG = Key.CourseModule
+
+const CourseModuleSchema = new Schema<ICourseModule>(
+  {
+    course: {
+      type    : Schema.Types.ObjectId,
+      ref     : Key.Course,
+      required: true,
+      index   : true
+    },
+    title: {
+      type    : String,
+      required: [true, SCHEMA.NAME],
+      trim    : true
+    },
+    labelKey: {
+      type    : String,
+      required: [true, SCHEMA.NAME],
+      trim    : true
+    },
+    description: {
+      type   : String,
+      trim   : true,
+      default: ''
+    },
+    order: {
+      type   : Number,
+      default: 0
+    }
+  },
+  {
+    timestamps: true,
+    collection: TAG
+  }
+)
+
+CourseModuleSchema.index({ course: 1, order: 1 })
+
+const CourseModule = model(TAG, CourseModuleSchema)
+export default CourseModule
