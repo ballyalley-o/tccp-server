3ac2811bfadc59c928fb98b75a83c06c98deaf4b
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-30 15:06:58 +1000

    [refactor]: learning event constants

---
+++ src/constant/enum/event.ts
@@ -0,0 +1,10 @@
+export const LEARNING_EVENT =  [
+        'lesson_started',
+        'lesson_completed',
+        'quiz_passed',
+        'assignment_submitted',
+        'resource_viewed',
+        'discussion_posted',
+        'login',
+        'badge_earned'
+      ] as LearningEventType[]
