81a6b297bb36ed2ec1555d8fffbe39ccb17bacd3
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-30 15:48:04 +1000

    [feat]: added Skill category

---
+++ src/model/SkillCategory.ts
@@ -0,0 +1,50 @@
+import mongoose   ,        { Schema, model } from 'mongoose'
+import { Key, SCHEMA } from '@constant/enum'
+
+const TAG = Key.SkillCategory
+
+const SkillCategorySchema = new Schema<ISkillCategory>(
+  {
+    name: {
+      type    : String,
+      required: [true, SCHEMA.NAME],
+      trim    : true,
+      unique  : true
+    },
+    labelKey: {
+      type    : String,
+      required: [true, SCHEMA.NAME],
+      trim    : true,
+      unique  : true
+    },
+    description: {
+      type   : String,
+      trim   : true,
+      default: ''
+    },
+    slug: {
+      type    : String,
+      required: true,
+      unique  : true,
+      index   : true
+    },
+    order: {
+      type   : Number,
+      default: 0
+    }
+  },
+  {
+    timestamps: true,
+    collection: TAG
+  }
+)
+
+SkillCategorySchema.pre(Key.Save, function (next) {
+  this.slug = String(this.name).toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-_]/g, '')
+  next()
+})
+
+SkillCategorySchema.index({ order: 1 })
+
+const SkillCategory = model(TAG, SkillCategorySchema)
+export default SkillCategory
