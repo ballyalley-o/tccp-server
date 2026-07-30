c00c0f5dc880aa1b3f3dd74dd698a924b5451234
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-30 15:29:31 +1000

    barrel

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,9 +1,9 @@
 export { default as AuthController } from './auth'
 export { default as bootcampController } from './bootcamp'
-export { default as CourseController } from './course'
+export { default as CourseController } from './course/course'
 export { default as EnrollmentController } from './enrollment'
 export { default as FeedbackController } from './feedback'
 export { default as UserController } from './user'
-export { default as DashboardController } from './dashboard'
+export { default as DashboardController } from './dashboard/dashboard'
 
 export { default as SystemController } from './system/system'
