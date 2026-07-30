b5c3ffc8eaacd0b5993404698102bc842b2089e5
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-07-30 16:13:52 +1000

    barrel

--- src/route/course/index.ts
+++ src/route/course/index.ts
@@ -1,9 +1,16 @@
-import { Application } from 'express'
-import { PathDir } from '@route/dir'
-import courseRoute from '@route/course/course'
+import { Application }    from 'express'
+import { PathDir }        from '@route/dir'
+
+import courseRoute        from './course'
+import courseLectureRoute from './lecture'
+import courseModuleRoute  from './module'
+import courseQuizRoute    from './quiz'
 
 const linkCourseRoute = (app: Application) => {
   app.use(PathDir.COURSE, courseRoute)
+  app.use(PathDir.COURSE_LECTURE, courseLectureRoute)
+  app.use(PathDir.COURSE_MODULE, courseModuleRoute)
+  app.use(PathDir.COURSE_QUIZ, courseQuizRoute)
 }
 
 export { linkCourseRoute }
