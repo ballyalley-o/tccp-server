c67835d4ab580fea8838b50cdcc0f503cb9d98a0
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-07 12:00:11 +1000

    [mock]: role collection added

---
+++ src/mock/role-collection.ts
@@ -0,0 +1,24 @@
+import ObjectID from 'bson-objectid'
+
+const roleCollection = [
+  {
+    _id     : ObjectID('5c3b08ae6d663a3c4e8f3566'),
+    name    : 'admin',
+    label   : 'Administrator',
+    metadata: { builtin: true }
+  },
+  {
+    _id     : ObjectID('5c3b08ae6d663a3c4e8f3567'),
+    name    : 'trainer',
+    label   : 'Trainer',
+    metadata: { builtin: true }
+  },
+  {
+    _id     : ObjectID('5c3b08ae6d663a3c4e8f3568'),
+    name    : 'student',
+    label   : 'Student',
+    metadata: { builtin: true }
+  }
+]
+
+export default roleCollection
