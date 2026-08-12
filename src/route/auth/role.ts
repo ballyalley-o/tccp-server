9a9f4e3c002aeeaac061a5cc9fd9685b22e72b7c
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-12 10:49:47 +1000

    propagations on the recent changes in struct

--- src/route/auth/role.ts
+++ src/route/auth/role.ts
@@ -1,17 +1,19 @@
-import { Router }             from 'express'
-import { protect, authorize } from '@route/guard'
-import { SkillController }    from '@controller/skill'
-import { PathDir }            from '@route/dir'
+import { Router }                   from 'express'
+import { Role }                     from '@model'
+import { advancedResult }           from '@middleware'
+import { RoleController }           from '@controller/auth'
+import { PathDir }                  from '@route/dir'
+import { protect, authorizeAction } from '@route/guard'
 
 const router = Router({ mergeParams: true })
 
-router.get(PathDir.ROOT, SkillController.getSkills)
-router.get(PathDir.ID, SkillController.getSkill)
-router.post(PathDir.ROOT, protect, authorize('trainer', 'admin'), SkillController.createSkill)
-router.put(PathDir.ID, protect, authorize('trainer', 'admin'), SkillController.updateSkill)
-router.delete(PathDir.ID, protect, authorize('trainer', 'admin'), SkillController.deleteSkill)
+router.get(PathDir.ROOT, advancedResult(Role, ''), RoleController.getRoles)
+router.get(PathDir.ID, RoleController.getRole)
+router.post(PathDir.ROOT, protect, authorizeAction('create:role'), RoleController.createRole)
+router.put(PathDir.ID, protect, authorizeAction('update:role'), RoleController.updateRole)
+router.delete(PathDir.ID, protect, authorizeAction('delete:role'), RoleController.deleteRole)
 
 /**
- * @path - {baseUrl}/api/{appVer}/auth
+ * @path - {baseUrl}/api/{appVer}/auth/role
  */
 export default router
