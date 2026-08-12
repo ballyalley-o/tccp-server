419008adcc48f995b98a8d1fa27ded4819ef168a
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-12 10:58:56 +1000

    [fix]

--- src/route/auth/user.ts
+++ src/route/auth/user.ts
@@ -10,8 +10,8 @@ const router = Router({ mergeParams: true })
 /**
  * @path - {baseUrl}/api/v0.1/auth/user
  */
-router.get(PathDir.ROOT, protect, authorizeAction('manage:users'), advancedResult(User, 'email'), UserController.getUsers)
-router.get(PathDir.ID, protect, authorizeAction('manage:users'), UserController.getUser)
+router.get(PathDir.ROOT, protect, authorizeAction('manage:user'), advancedResult(User, 'email'), UserController.getUsers)
+router.get(PathDir.ID, protect, authorizeAction('manage:user'), UserController.getUser)
 router.post(PathDir.ROOT, protect, authorizeAction('create:user'), UserController.createUser)
 router.put(PathDir.ID, protect, authorizeAction('update:user'), UserController.updateUser)
 router.delete(PathDir.ID, protect, authorizeAction('delete:user'), UserController.deleteUser)
