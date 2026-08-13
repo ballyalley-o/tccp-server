49982a7ae9b578e9346a4c88e073fbabe2a89e0d
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-13 15:30:07 +1000

    [refactor]: transformed enum to const

---
+++ src/constant/aggregate.ts
@@ -0,0 +1,11 @@
+const Aggregate = {
+  Bootcamp      : '$bootcamp',
+  User          : '$user',
+  Course        : '$course',
+  Feedback      : '$feedback',
+  Rating        : '$rating',
+  AverageRating : '$averageRating',
+  Tuition       : '$tuition',
+} as const
+
+export default Aggregate
