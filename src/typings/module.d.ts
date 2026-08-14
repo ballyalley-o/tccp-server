5b647dd9c0c2a2f844298d921abddaa56190e1b0
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-14 23:36:51 +1000

    [refactor]: restructure Module types and update submodule references

--- src/typings/module.d.ts
+++ src/typings/module.d.ts
@@ -1,20 +1,23 @@
 import { MODULE } from "@config/module"
 
 declare global {
-    type ModuleType = {
+    namespace Module {
+    type Key = keyof typeof MODULE
+
+    type ConfigType = {
         name      : string
-        labelKey  : string                     // TODO: impl LocaleKey
-        path      : string
+        labelKey  : string                      // TODO: impl LocaleKey
         action    : readonly ActionType[]
-        submodule?: Record<string, ModuleType>
+        submodule?: Record<string, ConfigType>
     }
 
-    type SubmoduleType<M extends keyof typeof MODULE> =
-        typeof MODULE[M]['submodule'] extends infer S
-            ? S extends Record<string, unknown>
-                ? keyof S
-                : never
-            : never
+    type SubmoduleKey<M extends Key> = (typeof MODULE)[M]['submodule'] extends infer S
+        ? S extends Record<string, unknown>
+          ? keyof S
+          : never
+        : never
+    }
+
 
     type ResourceName<T> = T extends { name: infer N extends string }
       ? N | (T extends { submodule: infer S } ? (S extends Record<string, unknown> ? ResourceName<S[keyof S]> : never) : never)
