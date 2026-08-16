bdb2051ec8a1d9a918718eec0198803d5595e2bb
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-16 16:15:31 +1000

    [cleanup]

--- src/typings/app.d.ts
+++ src/typings/app.d.ts
@@ -1,4 +1,11 @@
-declare type AppHealthStatus         = 'healthy' | 'degraded' | 'unhealthy'
-declare type AppUserRoleType         = 'user' | 'trainer' | 'admin'
-declare type AppUserRoleExtendedType = AppUserRoleType | 'guest'
-declare type AppEnrollmentType       = 'enrolled' | 'in_progress'| 'completed' | 'dropped'
+declare global {
+    type AppHealthStatus         = 'healthy' | 'degraded' | 'unhealthy'
+    type AppUserRoleType         = 'user' | 'trainer' | 'admin'
+    type AppUserRoleExtendedType = AppUserRoleType | 'guest'
+    type AppEnrollmentType       = 'enrolled' | 'in_progress' | 'completed' | 'dropped'
+    type AppThemeType            = 'primary' | 'success' | 'info' | 'warning' | 'error'
+    type AppEnvType              = 'production' | 'development' | 'test'
+}
+
+
+export {}
