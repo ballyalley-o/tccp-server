62db4a9b7896e301b037edf00c495703c56a8535
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-17 10:51:18 +1000

    'any' added for resource on permissions for admin usage

--- src/typings/permission.d.ts
+++ src/typings/permission.d.ts
@@ -2,7 +2,7 @@ import { ACTION } from "@config/permission.config"
 
 declare global{
     type ActionType     = (typeof ACTION)[number]
-    type PermissionType = `${ActionType}:${ResourceType}`
+    type PermissionType = `${ActionType}:${ResourceType | 'any'}`
 }
 
 export {}
