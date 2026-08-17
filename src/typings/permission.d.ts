2eeceafa79dc915f87fba0d6dbda177b811cedec
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-17 11:23:20 +1000

    [formatting]

--- src/typings/permission.d.ts
+++ src/typings/permission.d.ts
@@ -1,6 +1,6 @@
 import { ACTION } from "@config/permission.config"
 
-declare global{
+declare global {
     type ActionType     = (typeof ACTION)[number]
     type PermissionType = `${ActionType}:${ResourceType | 'any'}`
 }
