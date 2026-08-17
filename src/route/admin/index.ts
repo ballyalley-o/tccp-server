cff304413027342bbefa57c290b2949bfd2eb0a3
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-17 20:40:20 +1000

    [formatting]

--- src/route/admin/index.ts
+++ src/route/admin/index.ts
@@ -1,6 +1,6 @@
 import { Application } from 'express'
-import { PathDir } from '@route/dir'
-import systemRoute from '@route/admin/system/system'
+import { PathDir }     from '@route/dir'
+import systemRoute     from '@route/admin/system/system'
 
 const linkAdminRoute = (app: Application) => {
   app.use(PathDir.SYSTEM, systemRoute)
