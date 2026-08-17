7273ff7bd749b38103efcf8677412a0da17e31db
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-17 17:03:13 +1000

    removed mongoose def

--- src/typings/mongoose.d.ts
+++
@@ -1,9 +0,0 @@
-// declare namespace Mongoose {
-//   namespace Schema {
-//     namespace Types {
-//       type ObjectId = any
-//     }
-//   }
-// }
-
-// declare type ModelType = IBootcamp | IUser | IDefault | ICourse | ICourseExtended | IFeedback | IFeedbackExtended
