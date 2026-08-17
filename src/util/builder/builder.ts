0acf56f62a7df92d71d24785dce28f636eb84af0
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-17 23:13:04 +1000

    [formatting]: formatting and type fix

--- src/util/builder/builder.ts
+++ src/util/builder/builder.ts
@@ -1,5 +1,5 @@
-const  _trim                      = (s: string) => s.replace(/^\/+|\/+$/g, '')
-export const pathBuilder          = (...parts: string[]) => '/' + parts.map(_trim).join('/')
-export const  urlBuilder: UrlBuilderType = (base, ...parts) => {
+const  _trim                             = (s: string) => s.replace(/^\/+|\/+$/g, '')
+export const pathBuilder                 = (...parts: string[]) => '/' + parts.map(_trim).join('/')
+export const  urlBuilder: IAppUrlBuilder = (base, ...parts) => {
   return [_trim(base), ...parts.map(_trim)].join('/')
 }
