7c442c8cf2065290bbc3edefcfb10152e6dce16e
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-17 16:33:24 +1000

    [doc]: updated doc and implement the module config

--- src/route/skill/skill.ts
+++ src/route/skill/skill.ts
@@ -1,4 +1,5 @@
 import { Router }                   from 'express'
+import { MODULE }                   from '@config/module.config'
 import { SkillController }          from '@controller/skill'
 import { Skill }                    from '@model'
 import { advancedResult }           from '@middleware'
@@ -9,7 +10,7 @@ const router = Router({ mergeParams: true })
 
 router.get(PathDir.ROOT, advancedResult(Skill,
       {
-        path  : 'category',
+        path  : MODULE.Skill.submodule.SkillCategory.name,
         select: '_id name labelKey'
       }
     ), SkillController.getSkills)
@@ -18,4 +19,7 @@ router.post(PathDir.ROOT, protect, authorizeAction('create:skill'), SkillControl
 router.put(PathDir.ID, protect, authorizeAction('update:skill'), SkillController.updateSkill)
 router.delete(PathDir.ID, protect, authorizeAction('delete:skill'), SkillController.deleteSkill)
 
+/**
+ * @path - {baseUrl}/api/{apiVer}/skill
+ */
 export default router
