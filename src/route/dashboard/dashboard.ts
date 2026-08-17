11db16ad274ec3a3b64859dec4230710e0c2bc81
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-17 16:30:51 +1000

    [doc]: updated doc

--- src/route/dashboard/dashboard.ts
+++ src/route/dashboard/dashboard.ts
@@ -8,4 +8,7 @@ const router = Router({ mergeParams: true })
 router.get(PathDir.ROOT, DashboardController.getDashboard)
 router.post(PathDir.EVENT, protect, DashboardController.recordLearningEvent)
 
+/**
+ * @path - {baseUrl}/api/{apiVer}/dashboard
+ */
 export default router
