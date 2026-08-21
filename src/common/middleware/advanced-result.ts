b8e660d4005f3c87e12865048187b58f05786282
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-08-22 01:17:45 +1000

    [perf]: enhanced

--- src/common/middleware/advanced-result.ts
+++ src/common/middleware/advanced-result.ts
@@ -1,14 +1,55 @@
 import type { Request, Response, NextFunction } from 'express'
-import { Document, Model as MongooseModel }     from 'mongoose'
+import {
+  Document,
+  Model as MongooseModel,
+  PopulateOptions
+}                                                from 'mongoose'
 import RESPONSE                                 from '@common/constant/response'
+import { Code }                                 from '@common/constant/enum'
 import { REMOVE_FIELDS }                        from '@common/constant/remove-fields'
+import ErrorResponse                            from '@common/util/error-response'
 
 interface Model<T extends Document> extends MongooseModel<T> {}
 
-const advancedResult = (model: Model<any>, populate: any) => async (req: Request, res: Response, next: NextFunction) => {
+type PopulateConfig = string | PopulateOptions | Array<string | PopulateOptions>
+
+type AdvancedResultOptions = {
+  select ?: string[]
+  sort   ?: string[]
+  include?: Record<string, PopulateConfig>
+}
+
+const splitQueryParam = (value: unknown): string[] => {
+  if (!value || Array.isArray(value)) {
+    return []
+  }
+
+  return String(value)
+    .split(',')
+    .map((field) => field.trim())
+    .filter(Boolean)
+}
+
+const findUnknownFields = (requested: string[], allowed: string[]) =>
+  requested.filter((field) => !allowed.includes(field.replace(/^-/, '')))
+
+const applyPopulate = (query: ReturnType<Model<any>['find']>, populate: PopulateConfig) => {
+  if (typeof populate === 'string') {
+    return query.populate(populate)
+  }
+
+  return query.populate(populate)
+}
+
+const advancedResult = (
+  model   : Model<any>,
+  populate: PopulateConfig,
+  options : AdvancedResultOptions = {}
+) => async (req: Request, res: Response, next: NextFunction) => {
   const reqQuery = { ...req.query }
 
-  REMOVE_FIELDS.forEach((param) => delete reqQuery[param])
+  const removeFields = [...REMOVE_FIELDS, 'include']
+  removeFields.forEach((param) => delete reqQuery[param])
 
   const page       = parseInt(req.query.page as any, 10) || 1
   const limit      = parseInt(req.query.limit as any, 10) || 25
@@ -19,21 +60,52 @@ const advancedResult = (model: Model<any>, populate: any) => async (req: Request
   let dataQuery  = model.find(reqQuery)
 
   if (req.query.select) {
-    const fields    = (req.query.select as string).split(',').join(' ')
-          dataQuery = dataQuery.select(fields)
+    const fields = splitQueryParam(req.query.select)
+
+    if (!options.select) {
+      return next(new ErrorResponse('Select is not supported for this endpoint', Code.BAD_REQUEST))
+    }
+
+    const unknownFields = findUnknownFields(fields, options.select)
+    if (unknownFields.length) {
+      return next(new ErrorResponse(`Unsupported select fields: ${unknownFields.join(', ')}`, Code.BAD_REQUEST))
+    }
+
+    dataQuery = dataQuery.select(fields.join(' '))
   }
 
   if (req.query.sort) {
-    const sortBy    = (req.query.sort as string).split(',').join(' ')
-          dataQuery = dataQuery.sort(sortBy)
+    const sortBy = splitQueryParam(req.query.sort)
+
+    if (!options.sort) {
+      return next(new ErrorResponse('Sort is not supported for this endpoint', Code.BAD_REQUEST))
+    }
+
+    const unknownFields = findUnknownFields(sortBy, options.sort)
+    if (unknownFields.length) {
+      return next(new ErrorResponse(`Unsupported sort fields: ${unknownFields.join(', ')}`, Code.BAD_REQUEST))
+    }
+
+    dataQuery = dataQuery.sort(sortBy.join(' '))
   } else {
     dataQuery = dataQuery.sort('name')
   }
 
   dataQuery = dataQuery.skip(startIndex).limit(limit)
 
-  if (populate) {
-    dataQuery = dataQuery.populate(populate)
+  if (req.query.include) {
+    const includes = splitQueryParam(req.query.include)
+
+    for (const include of includes) {
+      const populateOption = options.include?.[include]
+      if (!populateOption) {
+        return next(new ErrorResponse(`Unsupported include: ${include}`, Code.BAD_REQUEST))
+      }
+
+      dataQuery = applyPopulate(dataQuery, populateOption)
+    }
+  } else if (populate) {
+    dataQuery = applyPopulate(dataQuery, populate)
   }
 
   dataQuery = dataQuery.lean()
